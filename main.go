197ad2f0ccdb050ee4306206330dbb60344a7de2
SAUL FRANCISCO FRANCISCO <saul.ruiz2920@alumnos.udg.mx>
2020-09-29 17:00:21 +0000

    e number

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+  var n int
+  fmt.Scan(&n)
+  var e float64 = 1
+  var fact int64 = 1
+  for i := 1; i <= n ; i++ {
+    fact *= int64(i)
+    e += float64(float64(1) / float64(fact))
+  }
+  fmt.Println(e)
 }
